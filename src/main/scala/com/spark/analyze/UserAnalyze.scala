034ab80dd7030819d2b49d9185488d3d2113b8ed
Administrator <Administrator@HYY-COMPANY>
2019-10-21 10:08:08 +0800

    增加电影前10，用户分布，影片的数量

--- src/main/scala/com/spark/analyze/UserAnalyze.scala
+++ src/main/scala/com/spark/analyze/UserAnalyze.scala
@@ -3,17 +3,11 @@ package com.spark.analyze
 import java.io.{BufferedReader, InputStreamReader}
 import java.util.Properties
 import java.util.zip.ZipInputStream
-
-import com.spark.mode.DataModel.TopNMovie
 import org.apache.spark.input.PortableDataStream
-import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql._
 
 
-/**
-  * 求前电影前10的访问量
-  */
 object UserAnalyze {
   def main(args: Array[String]): Unit = {
     //构建spark 上下文
@@ -31,41 +25,37 @@ object UserAnalyze {
     val distinctRDD = dataRDD.distinct()
     //把RDD 转成DataFrame
     val movieDF: DataFrame = spark.read.json(distinctRDD)
-    import spark.implicits._
-    //获取name数据进行统计
-    val movieNameAndOne: RDD[(String, Int)] = movieDF.map(x => (x.getAs("name").toString, 1)).rdd
-  //  movieNameAndOne.map(x => TopNMovie(x._1, x._2))
-    //过滤空值，进行累加
-    val movieNameTotal: RDD[(String, Int)] = movieNameAndOne.filter(!_._1.equals("")).reduceByKey(_ + _)
-    //进行倒排序
-    val sortMovieName: RDD[(String, Int)] = movieNameTotal.sortBy(_._2, false)
-    val sortMovieNameDF = sortMovieName.toDF("movie_name","count")
-    sortMovieNameDF.createOrReplaceTempView("TopNMovie")
-    val sortMovieTop10: Dataset[Row] = sortMovieNameDF.limit(10)
-    saveToMysql(sortMovieTop10, "TopNMovie")
+    movieDF.createOrReplaceTempView("Movie")
+    //影片的排名
+    var textMoviesql = "select name,cid,count(*) as num from movie  group by name,cid having cid=1 and trim(name) != '' order by num desc"
+    val movieTop10DF: DataFrame = spark.sql(textMoviesql)
+    // movieTop10DF.show(10)
+    saveToMysql(movieTop10DF.limit(10), "TopNMovie")
+
+    //影片类型喜好
+    var textLikeTypeSql = "select cid,count(*) as num from movie  group by cid having cid>0 and cid!=515  order by num desc"
+    val moviewTypeCount: DataFrame = spark.sql(textLikeTypeSql)
+    saveToMysql(moviewTypeCount.limit(10), "MovieTypeCount")
 
 
-    //进行统计用户分布的各个省
-    val movieProvinceAndOne: RDD[(String, Int)] = movieDF.map(x => (x.getAs("province").toString, 1)).rdd
-
-    val movieProvinceTotal: RDD[(String, Int)] = movieProvinceAndOne.filter(!_._1.equals("")).reduceByKey(_ + _)
-
-    val movieProvinceDF = movieProvinceTotal.toDF("province_name","count")
-
-    movieProvinceDF.createOrReplaceTempView("TopNProvince")
-
-    saveToMysql(movieProvinceDF, "TopNProvince")
-
+    //进行统计各个省的用户
+    val textPerProvinceUserSql = "select distinct  uuid,province  from Movie group by uuid,province"
+    val userProvinceDF = spark.sql(textPerProvinceUserSql);
+    userProvinceDF.createOrReplaceTempView("userarea")
+    val textCountSql = "select province, count(*) as num from userarea group by province  order by num desc"
+    val userProvinceCountDF: DataFrame = spark.sql(textCountSql)
+    // userProvinceCountDF.show(20)
+    saveToMysql(userProvinceCountDF, "TopNProvince")
     spark.stop()
 
 
   }
 
   /**
-    * 保存到mysql
-    *
-    * @param dtData
-    */
+   * 保存到mysql
+   *
+   * @param dtData
+   */
   private def saveToMysql(dtData: DataFrame, mysqlName: String) = {
 
     // 9.把结果保存在mysql表中
@@ -77,10 +67,10 @@ object UserAnalyze {
 
 
   /**
-    * 从hdfs 上加载数据 zip数据
-    *
-    * @param sc
-    */
+   * 从hdfs 上加载数据 zip数据
+   *
+   * @param sc
+   */
   def loadZipDataFromHdfs(sc: SparkContext) = {
     //hdfs地址
     val baseDir = "hdfs://192.168.1.123:8020/movie"
--- src/main/scala/com/spark/mode/DataModel.scala
+++ src/main/scala/com/spark/mode/DataModel.scala
@@ -3,23 +3,41 @@ package com.spark.mode
 object DataModel {
 
   /**
-    * 用户访问动作表
-    *
-    * @param uuid
-    * @param adType   用户的ID
-    * @param name     Session的ID
-    * @param province 某个页面的ID
-    *
-    */
-  case class Movie(uuid: String, adType: String, name: String, province: String)
+   * 用户访问动作表
+   *
+   * @param uuid
+   * @param adType   用户的ID
+   * @param name     Session的ID
+   * @param province 某个页面的ID
+   *
+   */
+  case class Movie(uuid: String, cid: Int, adType: String, name: String, province: String)
 
   /**
-    *
-    * @param movieName  电影名字
-    * @param num 点击次数
-    */
-  case class TopNMovie(movieName:String, num:Long)
+   * 用户区域表
+   */
 
-  case  class TopNProvince(province:String,num:Long)
+  case class userarea(uuid: String, province: String, num: Long)
 
+  /**
+   *
+   * @param movieName 电影名字
+   * @param num       点击次数
+   */
+  case class TopNMovie(movieName: String, cid: Int, num: Long)
+
+  /**
+   * 各省的用户量
+   *
+   * @param province
+   * @param num
+   */
+  case class TopNProvince(province: String, num: Long)
+
+  /**
+   * 各种电影类型的总数
+   * @param uuid
+   * @param movieName
+   */
+  case class MovieTypeCount(uuid:Int,movieName:String)
 }
